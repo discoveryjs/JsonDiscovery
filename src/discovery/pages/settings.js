0e2d68526fba2bd0757738ff4d79921250a1fdb5
Roman Dvornov <rdvornov@gmail.com>
2024-11-24 17:39:29 +0100

    Initial refactor of settings page

--- src/discovery/pages/settings.js
+++ src/discovery/pages/settings.js
@@ -30,7 +30,7 @@ export default host => {
             className: 'dark-mode-switcher',
             name: 'darkmode',
             label: 'Color schema',
-            postRender: (el, opts, data, context) => {
+            postRender(el, opts, data) {
                 let selfValue;
 
                 detachToggleDarkMode();
@@ -48,8 +48,9 @@ export default host => {
                         onChange: value => {
                             selfValue = value;
                             host.darkmode.set(value);
-                            saveSettings(Object.assign(context, { darkmode: value }));
+                            saveSettings({ ...data, darkmode: value });
                         },
+                        name: 'darkmode',
                         value: newValue,
                         data: [
                             { value: false, text: 'Light' },
@@ -65,35 +66,31 @@ export default host => {
             htmlType: 'number',
             htmlMin: 0,
             name: 'expandLevel',
-            value: '#.expandLevel+""', // input doesn't allow non-string values, and #.expandLevel is a number
+            value: 'expandLevel+""', // input doesn't allow non-string values, and #.expandLevel is a number
             label: 'Expand Level'
         }
     ].map(content => ({ view: 'fieldset', content }));
 
-    host.page.define('settings', async function(el, data) {
-        if (!data) {
-            return;
-        }
-
-        const settings = await host.query('"getSettings".callAction()', data);
-
-        host.view.render(el, [
-            'h1:"JsonDiscovery settings"',
-            {
-                view: 'context',
-                modifiers,
-                content: [
-                    {
-                        view: 'button-primary',
-                        content: 'text:"Save"',
-                        onClick: (el, data, context) => {
-                            saveSettings(context);
-                        }
+    host.page.define('settings', [
+        'h1:"JsonDiscovery settings"',
+        {
+            view: 'context',
+            data: '"getSettings".callAction()',
+            modifiers,
+            content: [
+                {
+                    view: 'button-primary',
+                    content: 'text:"Save"',
+                    onClick(el, data, context) {
+                        saveSettings({
+                            darkmode: host.darkmode.value,
+                            expandLevel: Number(context.expandLevel)
+                        });
                     }
-                ]
-            }
-        ], {}, settings);
-    });
+                }
+            ]
+        }
+    ]);
 
     /**
      * Saves settings to storage
